2b942a6e3dd0ab2e7296ba05ca014e2e36c1c309
Xerxes Battiwalla <xerxes.battiwalla@bionicvis.com>
2020-05-08 16:06:27 +1000

    Added an initial module and test func

---
+++ AddressBook/Library.fs
@@ -0,0 +1,25 @@
+﻿namespace AddressBook
+
+//type PersonalContact =
+//    | Name of string
+    
+type Person = {
+    Name: string
+}
+    
+type Contact =
+    | PersonalContact of Person
+    
+type AddressBook = Contact list
+
+module Test =
+    let test1 =
+        let luigi = { Name = "Luigi" }
+        let mario = { Name = "Mario" }
+        let contact1 = PersonalContact luigi
+        let contact2 = PersonalContact mario
+        let addressBook = [contact1; contact2]
+        
+        addressBook
+        |> List.iter (function
+            | PersonalContact c -> printfn "Name: %s" c.Name)
