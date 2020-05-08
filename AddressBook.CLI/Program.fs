fb87b05d62de7d6e16d87a924c71e469c27d3495
Xerxes Battiwalla <xerxes.battiwalla@bionicvis.com>
2020-05-08 16:16:55 +1000

    Added basic program

--- AddressBook.CLI/Program.fs
+++ AddressBook.CLI/Program.fs
@@ -1,8 +1,18 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open AddressBook
 
 [<EntryPoint>]
 let main argv =
+    let mutable (addressBook: AddressBook) = []
+    printf "Enter Name: "
+    let nameString = Console.ReadLine ()
+    let contact = PersonalContact { Name = nameString }
+    addressBook <- contact :: addressBook
     
+    printfn "...Listing all contacts..."
+    addressBook
+    |> List.iter (function
+        | PersonalContact c -> printfn "Contact Name: %s" c.Name )
     0 // return an integer exit code
