09267a2ceeec9639b5892bb6bf365ce91924c14d
Xerxes Battiwalla <xerxesb@gmail.com>
2020-05-27 10:29:03 +1000

    Yuuuuge commit to include WPF.Elmish * paket.dependencies stays in project root * paket.references goes into each project * run dn paket install to install deps into each project
    This inclusion of XAML view uses the following
    * A C# library project to hold all the View components
    * A F# console app with the console window hidden. This is the entrypoint
    
    The view is just a dumb window with no bindings (that's still to come)
    The model is a dummy placeholder which I derived from the WPF.Elmish tutorial.

---
+++ AddressBook.Elmish/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Elmish
+open Elmish.WPF
+open AddressBook.Elmish.Views
+
+type Model =
+    { Text: string }
+
+let init =
+    { Text = "hello" }
+
+type Msg =
+    | DoNothing
+
+let update msg m =
+    m
+
+let bindings () : Binding<Model, Msg> list =  []
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv =
+    Program.mkSimpleWpf (fun _ -> init) update bindings
+    |> Program.runWindow(MainWindow())
