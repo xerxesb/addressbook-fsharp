df437aaecc7e31e12b4cc540b8698592bbe53a79
Xerxes Battiwalla <xerxesb@gmail.com>
2020-05-27 13:45:29 +1000

    Added greeting that updates on button press

--- AddressBook.Elmish/Program.fs
+++ AddressBook.Elmish/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 open Elmish
 open Elmish.WPF
 open AddressBook.Elmish.Views
@@ -12,17 +10,20 @@ let init =
     { Greeting = "Hello from VM!" }
 
 type Msg =
-    | DoNothing
+    | Greet
 
 let update msg m =
-    m
+    match msg with
+    | Greet -> { m with Greeting = "You pressed the button!" }
 
 let bindings () : Binding<Model, Msg> list =  [
-    "Greeting" |> Binding.oneWay (fun m -> m.Greeting)
+    "GreetingMessage" |> Binding.oneWay (fun m -> m.Greeting)
+    "Greet" |> Binding.cmd Greet
 ]
 
 [<EntryPoint>]
 [<STAThread>]
 let main argv =
     Program.mkSimpleWpf (fun _ -> init) update bindings
+    |> Program.withConsoleTrace
     |> Program.runWindow(MainWindow())
