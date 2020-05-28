758c4d236febd89a1783835806980aa5cffcc73a
Xerxes Battiwalla <xerxesb@gmail.com>
2020-05-28 14:40:01 +1000

    Added edit button to screen   * Button responds to selected element and enables/disables accordingly
    Next task is to display popover to edit a contact

--- AddressBook.Elmish/Program.fs
+++ AddressBook.Elmish/Program.fs
@@ -16,31 +16,32 @@ let init =
 type Msg =
     | LoadBook
     | Select of int option
+    | EditSelectedContact
 
 let update msg m =
     match msg with
     | LoadBook -> { m with Book = AddressBook.Core.Persistence.fetchAllAddresses () }
-    | Select x -> System.Diagnostics.Trace.WriteLine <| sprintf "Selected model with id %A" x
-                  { m with SelectedId = x }
-
+    | EditSelectedContact -> m
+    | Select x -> { m with SelectedId = x }
+    
 let bindings () : Binding<Model, Msg> list =  [
     "LoadBook" |> Binding.cmd LoadBook
+    "EditContact" |> Binding.cmd EditSelectedContact
     "Addresses" |> Binding.subModelSeq(
           (fun m -> m.Book),
           (fun e -> (Contact.getPerson e).Id),
           (fun () -> [
-                "Id" |> Binding.oneWay (fun (_, e) -> (Contact.getPerson e).Id)
+                "Id" |> Binding.oneWay (fun (model, e) -> (Contact.getPerson e).Id)
                 "FirstName" |> Binding.oneWay (fun (_, e) -> (Contact.getPerson e).FirstName)
                 "LastName" |> Binding.oneWay (fun (_, e) -> (Contact.getPerson e).LastName)
                 "Age" |> Binding.oneWay (fun (_, e) -> (Contact.getPerson e).Age)
                 "Email" |> Binding.oneWay (fun (_, e) -> (Contact.getPerson e).Email)
-          ])
-          
-      )
+          ]))
+    "CanEditContact" |> Binding.oneWay (fun model -> model.SelectedId <> None)
     // The api docs say not to use this method and instead use binding.twoway, but i couldnt get it to work
     // It spits out binding errors to the trace
-//    "SelectedPerson" |> Binding.twoWay ((fun m -> m.SelectedId), (fun v -> Select v))
-    "SelectedPerson" |> Binding.subModelSelectedItem("Addresses", (fun m -> m.SelectedId), Select)
+//    "SelectedContact" |> Binding.twoWay ((fun m -> m.SelectedId), (fun v -> Select v))
+    "SelectedContact" |> Binding.subModelSelectedItem("Addresses", (fun m -> m.SelectedId), Select)
 ]
 
 [<EntryPoint>]
